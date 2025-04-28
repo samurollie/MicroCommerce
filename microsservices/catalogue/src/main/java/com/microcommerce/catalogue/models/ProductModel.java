ed9bc42de75b768592023c3ad1d4c2068eb924bf
Samuel Lucas <slvlb@ic.ufal.br>
2025-04-28 17:55:42 -0300

    Catalogue: Add product Rating

--- microsservices/catalogue/src/main/java/com/microcommerce/catalogue/models/ProductModel.java
+++ microsservices/catalogue/src/main/java/com/microcommerce/catalogue/models/ProductModel.java
@@ -1,17 +1,24 @@
 package com.microcommerce.catalogue.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
+import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.validator.constraints.Length;
 
 import java.util.Date;
 
 @Entity
 @Table(name = "products")
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,12 +41,23 @@ public class ProductModel {
     private int quantity;
 
     @NotNull
+    @ColumnDefault("0")
+    @Max(value = 5)
+    @Min(value = 0)
+    private float rating;
+
+    @NotNull
     @Enumerated(EnumType.STRING)
     private ProductType type;
 
     @ManyToOne
     private SellerModel seller;
 
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
     private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
     private Date updatedAt;
 }
