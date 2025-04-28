d3263e768beb3118162a169d7bd0f2c54f9758e9
Samuel Lucas <slvlb@ic.ufal.br>
2025-04-28 15:32:13 -0300

    Remove flyway

--- microsservices/catalogue/src/main/java/com/microcommerce/catalogue/models/UserModel.java
+++ microsservices/catalogue/src/main/java/com/microcommerce/catalogue/models/UserModel.java
@@ -2,18 +2,15 @@ package com.microcommerce.catalogue.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
-@Table(name = "user")
 @Entity
 public class UserModel {
     @Id
     private Long id;
-
     private String name;
     private String password;
     private String email;
