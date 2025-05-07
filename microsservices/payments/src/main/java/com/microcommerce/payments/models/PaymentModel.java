9ed7622297f6e7649dc366bc13d939fb2c236293
Ricardo Vinicius <ricardoviniciusaf@gmail.com>
2025-05-07 18:49:02 -0300

    feat: add payment methods and statuses to PaymentModel, update application configuration

--- microsservices/payments/src/main/java/com/microcommerce/payments/models/PaymentModel.java
+++ microsservices/payments/src/main/java/com/microcommerce/payments/models/PaymentModel.java
@@ -1,5 +1,11 @@
 package com.microcommerce.payments.models;
 
+import java.math.BigDecimal;
+import java.sql.Date;
+
+import com.microcommerce.payments.enums.PaymentMethod;
+import com.microcommerce.payments.enums.PaymentStatus;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,5 +23,22 @@ public class PaymentModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
 
+    @Column(name = "order_id", nullable = false, unique=true)
+    private String orderId;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
+
+    @Column(name = "amount", nullable = false)
+    private BigDecimal amount;
+
+    @Column(name = "payment_date", nullable = false)
+    private Date paymentDate;
+
+    @Column(name = "approved_date")
+    private Date approvedDate;
 }
