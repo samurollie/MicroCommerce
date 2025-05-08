eca5f068d558df3fa87a8693ae6ea3542194458f
Ricardo Vinicius <ricardoviniciusaf@gmail.com>
2025-05-08 04:14:49 -0300

    feat: add endpoint to retrieve delivery price by zip code

--- microsservices/delivery/src/main/java/com/microcommerce/delivery/controllers/DeliveryController.java
+++ microsservices/delivery/src/main/java/com/microcommerce/delivery/controllers/DeliveryController.java
@@ -1,10 +1,15 @@
 package com.microcommerce.delivery.controllers;
 
 import com.microcommerce.delivery.dto.CreateDeliveryDTO;
+import com.microcommerce.delivery.dto.DeliveryPriceDTO;
 import com.microcommerce.delivery.dto.PublicDeliveryDTO;
 import com.microcommerce.delivery.exception.DeliveryAlreadyExistentException;
 import com.microcommerce.delivery.services.DeliveryService;
+import feign.Response;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,5 +37,14 @@ public class DeliveryController {
         } catch (DeliveryAlreadyExistentException e) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
         }
+    }
+
+    @GetMapping("/price")
+    public ResponseEntity<DeliveryPriceDTO> getDeliveryPrice(@RequestParam("zipCode") @NotEmpty String zipCode) {
+        var price = deliveryService.getDeliveryPrice(zipCode);
+        DeliveryPriceDTO deliveryPriceDTO = new DeliveryPriceDTO();
+        deliveryPriceDTO.setPrice(price);
+
+        return ResponseEntity.status(HttpStatus.OK).body(deliveryPriceDTO);
     }
 }
---
+++ microsservices/delivery/src/main/java/com/microcommerce/delivery/dto/DeliveryPriceDTO.java
@@ -0,0 +1,8 @@
+package com.microcommerce.delivery.dto;
+
+import lombok.Data;
+
+@Data
+public class DeliveryPriceDTO {
+    private Double price;
+}
